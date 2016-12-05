e10e359ec15671631114428845914a6f5d851197
ptran021 <ptran021@ucr.edu>
2016-12-04 22:43:53 -0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,21 +1,33 @@
-setwd("C:/Users/patri/Desktop/project1")
-dat <- read.table("Household_power_consumption.txt",sep = ";",header = T,na.strings = "")
-
-
-##subset data
-dat <- dat[dat$Date %in% c("1/2/2007","2/2/2007"),]
-time <- strptime((paste(dat$Date,dat$Time, sep = " ")),"%d/%m/%Y %H:%M:%S")
-dat["setTime"]
-dat$setTime <- time
-
-##plot 4 
-labels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
-columnlines <- c("black","red","blue")
-par(mfrow=c(2,2))
-plot(dat$setTime, dat$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power")
-plot(dat$setTime, dat$Voltage, type="l", col="black", xlab="datetime", ylab="Voltage")
-plot(dat$setTime, dat$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-lines(dat$setTime, dat$Sub_metering_2, type="l", col="red")
-lines(dat$setTime, dat$Sub_metering_3, type="l", col="blue")
-legend("topright", bty="n", legend=labels, lty=1, col=columnlines)
-plot(dat$setTime, dat$Global_reactive_power, type="l", col="black", xlab="datetime", ylab="Global_reactive_power")
+df <- read.table("household_power_consumption.txt", skip = 66637, nrow = 2880, sep = ";")
+name <- sapply(read.table("household_power_consumption.txt", nrow = 1, sep = ";"), as.character)
+names(df) <- name
+df$DateTime <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+for( i in 3:9 ){
+    df[[i]] <- sapply(df[[i]], as.character)
+    df[[i]] <- sapply(df[[i]], as.numeric)
+}
+win.graph(200,200)
+par(mfcol = c(2,2))
+with(df, {plot(DateTime, Global_active_power, type = "n", xlab = "daytime", ylab = "Global Active Power (kilowatts)" , axes = F, frame.plot=TRUE)
+          axis(1, at=c(as.numeric(min(df$DateTime)), as.numeric(min(df$DateTime))+86400, as.numeric(min(df$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+          axis(2, yaxs = "r")
+          with(df, points(DateTime, Global_active_power, type = "l"))
+          plot(DateTime, Sub_metering_1, type = "n", xlab = "daytime", ylab = "Energy sub metering" , axes = F, frame.plot=TRUE)
+          axis(1, at=c(as.numeric(min(df$DateTime)), as.numeric(min(df$DateTime))+86400
+                       , as.numeric(min(df$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+          axis(2, yaxs = "r")
+          with(df, points(DateTime, Sub_metering_1, type = "l"))
+          with(df, points(DateTime, Sub_metering_2, type = "l", col = "red"))
+          with(df, points(DateTime, Sub_metering_3, type = "l", col = "blue"))
+          legend("topright", pch = "w", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))          
+          plot(DateTime, Voltage, type = "n", xlab = "daytime", ylab = "Voltage (volts)" , axes = F, frame.plot=TRUE)
+          axis(1, at=c(as.numeric(min(df$DateTime)), as.numeric(min(df$DateTime))+86400
+             , as.numeric(min(df$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+          axis(2, yaxs = "r")
+          with(df, points(DateTime, Voltage, type = "l"))
+          plot(DateTime, Global_reactive_power, type = "n", xlab = "daytime", ylab = "Global Reactive Power (kilowatts)" , axes = F, frame.plot=TRUE)
+          axis(1, at=c(as.numeric(min(df$DateTime)), as.numeric(min(df$DateTime))+86400
+             , as.numeric(min(df$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+          axis(2, yaxs = "r")
+          with(df, points(DateTime, Global_reactive_power, type = "l"))
+          })
